29fcc5fada28d08de04a5be7b6f72efae165e89a
lexi <alrad1744@gmail.com>
2026-03-24 12:41:58 +0000

    237.1

--- src/offsets.hpp
+++ src/offsets.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
 enum offsets {
-    CHECK_FOR_LUA = 0x8B7C0,
+    CHECK_FOR_LUA = 0x8B030,
 
-    LUA_REGISTER_DOUBLE = 0x357C20,
+    LUA_REGISTER_DOUBLE = 0x2F16E0,
 
-    CLIENT_INSTANCE = 0xE82A88,
+    CLIENT_INSTANCE = 0xE8AC38,
 };

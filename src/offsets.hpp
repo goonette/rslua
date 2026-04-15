9f2d71ef553266dbb7d7d815cc5bc9c11724f514
goonette <alrad1744@gmail.com>
2026-04-15 19:19:27 +0100

    237.5

--- src/offsets.hpp
+++ src/offsets.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
 enum offsets {
-    CHECK_FOR_LUA = 0x8B3E0,
+    CHECK_FOR_LUA = 0x8B620,
 
-    LUA_REGISTER_DOUBLE = 0x2F4840,
+    LUA_REGISTER_DOUBLE = 0x2F5410,
 
     CLIENT_INSTANCE = 0xE8EC78,
 };
